50284872e60a1495501e99595a79767a25193407
James Cutler <jamescutler1111@gmail.com>
2019-05-01 06:41:52 -0400

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    var topics = ["sunsets","puppies","kittens","back yard barbecues","a day at the beach with friends","fireworks"];
+    var counter = 0;
+    var gifKey = 'JN3sRoD1lGwkiwpRKuGOqTTYVcUZcHE6';
+    for (i=0;i<topics.length;i++){
+        var button = $('<button>');
+        button.attr('data-'+counter, "item"+counter);
+        button.attr('data-offset', 5);
+        button.addClass('gif-search');
+        button.text(topics[i]);
+        $('#gif-buttons').append(button);
+        counter++;
+    }
+    $(document).on('click','.gif-search',function(){
+        console.log($(this).get(0).dataset.offset);
+        var term = $(this).text();
+        var image;
+        var imageContainer;
+        console.log(term);
+        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+term+"&api_key="+gifKey+"&offset="+$(this).get(0).dataset.offset+"&limit=5");
+        xhr.done(function(data){
+            console.log(data);
+            var results = data.data;
+            for (i=0;i<results.length;i++){
+                console.log(results[i].images.original.url);
+                image = $('<img>');
+                imageContainer = $('<div>');
+                imageContainer.addClass('col-12 col-md-4 col-lg-2');
+                image.addClass('gif-result');
+                image.attr('src',results[i].images.original.url);
+                imageContainer.append(image);
+                $('.image-wrapper').append(imageContainer);
+            }
+            setTimeout(function(){
+            },100);
+        });
+        $(this).get(0).dataset.offset = (parseInt($(this).get(0).dataset.offset) + 5);
+    });
+    
+});
