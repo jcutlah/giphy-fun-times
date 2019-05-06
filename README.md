c75dca6d549d31aaab8b58ff8e236c5a0d9dba4a
James Cutler <jcutler@hubspot.com>
2019-05-05 21:56:30 -0400

    lots of fine-tuning the CSS

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # giphy-fun-times
+
+Giphy Fun Times is a mobile-responsive web app that allows you to search for .gif images from Giphy.com. If you like what you find, you will have the option to save and/or download the images. This is made possible through [Giphy's API](https://developers.giphy.com/docs/)
+
+A short list of pre-defined search terms are provided as clickable buttons. If you would like to choose your own search terms, you can do so by typing the search term into the text field on the page, and clicking 'Add Search Button'. This will add a clickable button to the existing series of buttons.
+
+Clicking a button will search the Giphy database and add 10 results to the page (as long as the search comes up with at least 10 results.) The preview images in these results will be a 'downsampled' version of the original gif, allowing them to be displayed much faster. These results will be formatted as a slider module, which utilizes the jQuery library, [slick.js](https://kenwheeler.github.io/slick/).
+
+When you click one of the search results, a lightbox container will feature the original gif image. On the lightbox, you will see options to 'add to favorites' or 'download' the image. 
+
+Clicking 'download' will prompt a download of the .gif image.
+
+Clicking 'add to favorites' will utilize the browser's local storage on the user's device. As long as the local storage is not deleted, the saved images will persist across different browser sessions (of the same browser) on that device.
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
     var counter = 0;
     var gifKey = 'JN3sRoD1lGwkiwpRKuGOqTTYVcUZcHE6';
     var isRepeatButton = false;
+
     function addButton(topic){
 
         $.each($('#gif-buttons .gif-search'), function(){
-            console.log($(this));
+            // console.log($(this));
             if (topic === $(this).text()){
-                console.log('topic/text match');
+                // console.log('topic/text match');
                 isRepeatButton = true;
                 return false;
             }
@@ -22,30 +23,50 @@ $(document).ready(function(){
             $('#gif-buttons').append(button);
             counter++;
         } else {
-            console.log('prevented repeat button');
+            // console.log('prevented repeat button');
         }
         isRepeatButton = false;
     }
     function addImagesFromLocalStorage(){
         var favorites = JSON.parse(localStorage.getItem('favoriteGiphys'));
         for (i=0;i<favorites.length;i++){
-            addImageToContainer('#favorites',favorites[i].favThumbnail,favorites[i].favOg,'gif-result favorite col-6 col-lg-3');
+            addImageToContainer('#favorites',favorites[i].favThumbnail,favorites[i].favOg,'gif-result favorite col-6 col-lg-3',{title:favorites[i].favTitle,rating:favorites[i].favRating});
         }
     }
-    function addImageToContainer(targetContainer, imgThumb, imgFull, classes){
+    function addImageToContainer(targetContainer, imgThumb, imgFull, classes, imgMeta){
         image = $('<img>');
         // image.addClass(classes);
         imageContainer = $('<div>');
+        if (imgMeta) {
+            // console.log(imgMeta.rating);
+            imageOverlay = $('<div>');
+            imageOverlay.addClass('gif-overlay');
+            imageMeta = $('<h4>');
+            imageMeta.addClass('img-meta');
+            imageMeta.html(imgMeta.title + "<br><small><em>(rated " + imgMeta.rating.toUpperCase() + ")</em></small>");
+            image.attr('data-title',imgMeta.title);
+            image.attr('data-rating',imgMeta.rating);
+            imageOverlay.append(imageMeta);
+            imageContainer.append(imageOverlay);
+            
+        }
         imageContainer.addClass(classes);
         image.attr('src',imgThumb);
         image.attr('data-og',imgFull)
         imageContainer.append(image);
         $(targetContainer).prepend(imageContainer);
     }
-    function addToFavorites(thumb, og){
+    function addToFavorites(thumb, og, meta){
         var favorites = $('#favorites');
-        addImageToContainer('#favorites',thumb,og,'gif-result favorite col-4 col-lg-3');
-        
+        addImageToContainer('#favorites',thumb,og,'gif-result favorite col-4 col-lg-3',meta);
+    }
+    function noFavoritesYet(){
+        var faves = $('#favorites .favorite');
+        if (faves.length === 0) {
+            $('#empty-favorites').show();
+        } else {
+            $('#empty-favorites').hide();;
+        }
     }
     function initSlick(target){
         $(target).slick({
@@ -70,20 +91,20 @@ $(document).ready(function(){
     function removeFavorite(og){
         var faves = JSON.parse(localStorage.getItem('favoriteGiphys'));
         for (i=0;i<faves.length;i++){
-            console.log(faves[i].favOg, og)
+            // console.log(faves[i].favOg, og)
             if (faves[i].favOg == og){
-                console.log('found match');
-                console.log(faves)
+                // console.log('found match');
+                // console.log(faves)
                 faves.splice(i, 1);
                 resetLocalStorage('favoriteGiphys',JSON.stringify(faves));
                 $.each($("#favorites .gif-result img"),function(){
-                    console.log($(this));
+                    // console.log($(this));
                     if ($(this).attr('data-og') === og){
                         $(this).parent().remove();
                     }
                 });
             } else {
-                console.log('no match');
+                // console.log('no match');
             }
         }
     }
@@ -100,39 +121,47 @@ $(document).ready(function(){
             currentVal.push(newVal);
             localStorage.setItem(key,JSON.stringify(currentVal));
         }
-        console.log(localStorage.getItem('favoriteGiphys'));
+        // console.log(localStorage.getItem('favoriteGiphys'));
     }
     for (i=0;i<topics.length;i++){
         addButton(topics[i]);
     }
     $(document).on('click','.gif-search',function(){
         var imageWrapper = $('.image-wrapper');
+        $('#loading-message').show();
+        $('#no-results').hide();
         if (imageWrapper.hasClass('slick-slider')) {
             imageWrapper.slick('unslick');
-            $('.image-wrapper').attr('style', "opacity: 0");
         }
-        console.log($(this).get(0).dataset.offset);
+        $('.image-wrapper').attr('style', "opacity: 0");
+
+        // console.log($(this).get(0).dataset.offset);
         var term = $(this).text();
         var image;
         var imageContainer;
-        console.log(term);
-        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q="+term+"&api_key="+gifKey+"&offset="+$(this).get(0).dataset.offset+"&limit=5");
+        // console.log(term);
+        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q="+term+"&api_key="+gifKey+"&offset="+$(this).get(0).dataset.offset+"&limit=10");
         xhr.done(function(data){
-            console.log(data);
+            // console.log(data);
             var results = data.data;
+            var imgMeta = {};
             for (i=0;i<results.length;i++){
-                console.log(results[i].images.original.url);
-                addImageToContainer(".image-wrapper",results[i].images.fixed_height_downsampled.url,results[i].images.original.url,'gif-result');
+                // console.log(results[i].images.original.url);
+                imgMeta.title = results[i].title;
+                imgMeta.rating = results[i].rating;
+                // console.log(imgMeta);
+                addImageToContainer(".image-wrapper",results[i].images.fixed_height_downsampled.url,results[i].images.original.url,'gif-result', imgMeta);
             }
             setTimeout(function(){
                 initSlick('.image-wrapper');
                 $('.image-wrapper').attr('style','opacity: 1');
-            },100);
+                $('#loading-message').hide();
+            },300);
         });
         $(this).get(0).dataset.offset = (parseInt($(this).get(0).dataset.offset) + 5);
     });
     $(document).on('click','.gif-result',function(){
-        console.log($(this));
+        // console.log($(this));
         if ($(this).hasClass('favorite')){
             $('.add-favorite').hide();
             $('.remove-favorite').show();
@@ -140,30 +169,43 @@ $(document).ready(function(){
             $('.add-favorite').show();
             $('.remove-favorite').hide();
         }
-        var src = $(this).children('.gif-result img').get(0).dataset.og;
-        console.log(src);
+        var data = $(this).children('.gif-result img').get(0).dataset;
+        // console.log(data);
         lbImage = $('#lightbox-image');
-        lbImage.attr('src',src);
+        lbImage.attr('src',data.og);
+        lbImage.attr('data-title',data.title);
+        lbImage.attr('data-rating',data.rating);
+        lbMeta = $('#lightbox-meta');
+        lbMeta.html(data.title + "<br><small><em>(rated " + data.rating.toUpperCase() + ")</em></small>")
+        $('#download-gif').attr('href',data.og);
+        $('#download-gif').attr('data-title',data.title);
         lbImage.attr('data-thumbnail',$(this).children('.gif-result img').attr('src'));
         $("#gif-lightbox-wrapper").show();
+        $("#body-overlay").show();
     });
     $(document).on('click','.close-lightbox',function(){
         $("#gif-lightbox-wrapper").hide();
+        $("#body-overlay").hide();
     });
     $(document).on('click','.add-favorite',function(){
         var favOg = $('#lightbox-image').attr('src');
-        var favThumbnail = $('#lightbox-image').get(0).dataset.thumbnail;
-        console.log(favOg, favThumbnail);
+        var favData = $('#lightbox-image').get(0).dataset;
+        // console.log(favOg, favData.thumbnail);
         if (isRepeatFave(favOg)){
             // 
         } else {
-            addToFavorites(favThumbnail, favOg);
+            addToFavorites(favData.thumbnail, favOg, {name: favData.title, rating: favData.rating});
             var favorite = {
                 favOg,
-                favThumbnail
+                favThumbnail: favData.thumbnail,
+                favTitle: favData.title,
+                favRating: favData.rating
             };
             setLocalStorage('favoriteGiphys',favorite);
+            $('#gif-lightbox-wrapper').hide();
+            $("#body-overlay").hide();
         }
+        noFavoritesYet();
     });
     $('#topic-submit').on('click',function(e){
         e.preventDefault();
@@ -174,14 +216,36 @@ $(document).ready(function(){
         var favOg = $('#lightbox-image').attr('src');
         removeFavorite(favOg);
         $('#gif-lightbox-wrapper').hide();
+        $("#body-overlay").hide();
+        noFavoritesYet()
+    });
+    $(document).on('click','#download-gif',function(e){
+        e.preventDefault();
+        var gifDownload = $(this).attr('href');
+        $.ajax({
+            url: gifDownload,
+            method: 'GET',
+            xhrFields: {
+                responseType: 'blob'
+            },
+            success: function (data) {
+                var a = document.createElement('a');
+                var url = window.URL.createObjectURL(data);
+                a.href = url;
+                a.download = $('#download-gif').attr('data-title');
+                a.click();
+                window.URL.revokeObjectURL(url);
+            }
+        });
     });
     if (localStorage.getItem('favoriteGiphys') === null){
-        console.log(localStorage);
+        // console.log(localStorage);
         localStorage.setItem('favoriteGiphys', '[]');
     } else {
-        console.log('yeah');
+        // console.log('yeah');
         addImagesFromLocalStorage();
-        // console.log(localStorage)
+        // // console.log(localStorage)
     }
+    noFavoritesYet();
     
 });
